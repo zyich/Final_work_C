367566753c783cd063c5c665f983df2b82a22d62
zyich <zuilinho@yandex.ru>
2023-11-01 18:51:01 +0100

    readme add

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,5 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+﻿
+
+
+/*
+Console.WriteLine("write number of elements in array :");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+string [] stringArray = new string[number];
+    
+    for (int i = 0; i < number; i++)
+        {
+            Console.Write($"Input word, number or symbol {i + 1}: ");
+            string input = Console.ReadLine();
+            if (string.IsNullOrEmpty(input))
+                {
+                    Console.WriteLine("it is not possible to make empty string, try again :");
+                    i--; 
+                }
+            else
+                {
+                    stringArray[i] = input;
+                }
+        }
+
+string  NewArrayString = "[" + string.Join(", ", stringArray) + "]";
+Console.WriteLine($" Your array : {NewArrayString}:");
+
+
+*/
+
+
+
+string[] stringArray = { "Russia", "God", "Windows", "12", "1" };
+Console.WriteLine("Исходный массив:");
+Console.WriteLine("[" + string.Join(", ", stringArray) + "]");
+
+Console.WriteLine("Элементы массива с 3 или менее символами:");
+string[] shortStrings = new string[stringArray.Length];
+int count = 0;
+for (int i = 0; i < stringArray.Length; i++)
+{
+    if (stringArray[i].Length <= 3)
+    {
+        shortStrings[count] = stringArray[i];
+        count++;
+    }
+}
+
+// Создайте новый массив с фактическим размером
+string[] trimmedShortStrings = new string[count];
+Array.Copy(shortStrings, trimmedShortStrings, count);
+
+Console.WriteLine("[" + string.Join(", ", trimmedShortStrings) + "]");
+Console.WriteLine(); // Переход на новую строку после вывода элементов
---
+++ task2/Program.cs
@@ -0,0 +1,45 @@
+﻿
+
+
+
+Console.WriteLine("write number of elements in array :");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+string [] stringArray = new string[number];
+    
+    for (int i = 0; i < number; i++)
+        {
+            Console.Write($"Input word, number or symbol {i + 1}: ");
+            string input = Console.ReadLine();
+            if (string.IsNullOrEmpty(input))
+                {
+                    Console.WriteLine("it is not possible to leave empty string, try again :");
+                    i--; 
+                }
+            else
+                {
+                    stringArray[i] = input;
+                }
+        }
+
+
+Console.WriteLine("Elements in array with 3 or less symbols:");
+string[] shortStrings = new string[stringArray.Length];
+int count = 0;
+for (int i = 0; i < stringArray.Length; i++)
+{
+    if (stringArray[i].Length <= 3)
+    {
+        shortStrings[count] = stringArray[i];
+        count++;
+    }
+}
+
+string[] cutShortStrings = new string[count];
+Array.Copy(shortStrings, cutShortStrings, count);
+
+Console.WriteLine("[" + string.Join(", ", cutShortStrings) + "]");
+Console.WriteLine();
